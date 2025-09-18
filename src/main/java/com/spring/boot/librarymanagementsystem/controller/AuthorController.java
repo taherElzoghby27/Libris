bd25d54f92be27b2d21e2617c3f26baba332d04d
taherElzoghby27 <tataamen678@gmail.com>
2025-09-18 21:51:37 +0300

    authors tested

--- src/main/java/com/spring/boot/librarymanagementsystem/controller/AuthorController.java
+++ src/main/java/com/spring/boot/librarymanagementsystem/controller/AuthorController.java
@@ -3,13 +3,11 @@ package com.spring.boot.librarymanagementsystem.controller;
 import com.spring.boot.librarymanagementsystem.dto.AuthorDto;
 import com.spring.boot.librarymanagementsystem.dto.SuccessDto;
 import com.spring.boot.librarymanagementsystem.service.AuthorService;
+import com.spring.boot.librarymanagementsystem.vm.AuthorResponseVm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -27,4 +25,34 @@ public class AuthorController {
                         body(authorService.createAuthor(authorDto))
         );
     }
+
+    @GetMapping("get-author")
+    public SuccessDto<ResponseEntity<AuthorDto>> getAuthor(@RequestParam Long authorId) {
+        return new SuccessDto<>(
+                ResponseEntity.ok(authorService.getAuthor(authorId))
+        );
+    }
+
+    @GetMapping("get-authors")
+    public SuccessDto<ResponseEntity<AuthorResponseVm>> getAuthors(@RequestParam int page, @RequestParam int size) {
+        return new SuccessDto<>(
+                ResponseEntity.ok(authorService.getAuthors(page, size))
+        );
+    }
+
+    @PutMapping("update-author")
+    public SuccessDto<ResponseEntity<AuthorDto>> updateAuthor(@Valid @RequestBody AuthorDto authorDto) {
+        return new SuccessDto<>(
+                ResponseEntity.ok(authorService.updateAuthor(authorDto))
+        );
+    }
+
+    @DeleteMapping("delete-author")
+    public SuccessDto<ResponseEntity<String>> deleteAuthor(@RequestParam Long id) {
+        authorService.deleteAuthor(id);
+        return new SuccessDto<>(
+                ResponseEntity.ok("Author deleted successfully")
+        );
+    }
+
 }
