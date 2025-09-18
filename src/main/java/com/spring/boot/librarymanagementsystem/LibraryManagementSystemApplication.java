f2b8dca532d5d73ee41c32fc38bc9e6b53d60291
taherElzoghby27 <tataamen678@gmail.com>
2025-09-18 03:36:40 +0300

    add annotations for use properties

--- src/main/java/com/spring/boot/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/spring/boot/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -2,8 +2,14 @@ package com.spring.boot.librarymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class LibraryManagementSystemApplication {
 
     public static void main(String[] args) {
